5ce006bd5c13d5a7aab25aaa82451c7e53e20d62
Sule <aybilenden@gmail.com>
2022-05-04 22:43:08 +0300

    Done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let textResult = document.querySelector(".header--text");
 let guessInput = document.getElementById("number");
 let checkButton = document.querySelector(".btn");
 let image = document.querySelector(".image");
+let numberDiv = document.querySelector(".numberDiv");
 
 let attempts = document.getElementById("attempts");
 
@@ -15,8 +16,11 @@ checkButton.addEventListener("click", checkClick);
 let randomNum = Math.floor(Math.random() * 101);
 console.log(randomNum);
 
+let tried_list = [];
 function checkClick(e) {
   let guessNumber = guessInput.value;
+  tried_list.push(" " + guessNumber)
+  numberDiv.innerText = "Tried Numbers : " +  tried_list
 
   if (guessNumber < 0 || guessNumber > 100) {
     alert("The number must be between 0 and 100");
@@ -38,6 +42,7 @@ function checkClick(e) {
       image.style.backgroundImage = "url(./img/gameover.gif)";
       checkButton.innerHTML = "RESTART";
       textResult.innerHTML = "Click RESTART!";
+      numberDiv.innerText = "True number : " + randomNum
       if (
         checkButton.addEventListener("click", () =>
           window.location.reload(false)
@@ -53,7 +58,7 @@ function checkClick(e) {
       image.style.backgroundImage = "url(./img/gameover.gif)";
       checkButton.innerHTML = "RESTART";
       textResult.innerHTML = "Click RESTART!";
-
+      numberDiv.innerText = "True number : " + randomNum
       if (
         checkButton.addEventListener("click", () =>
           window.location.reload(false)
@@ -65,6 +70,7 @@ function checkClick(e) {
     image.style.backgroundImage = "url(./img/winner.gif)";
     checkButton.innerHTML = "RESTART";
     textResult.innerHTML = "CONGRATULATIONS!!!"
+    numberDiv.innerText = "winner number : " + randomNum
     if (
       checkButton.addEventListener("click", () => window.location.reload(false))
     ) {
